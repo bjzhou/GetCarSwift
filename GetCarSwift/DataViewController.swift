f3960ec1592b03788805a35f819103b2848bc3f2
bjzhou <bjzhou1990@gmail.com>
2015-08-04 00:18:30 +0800

    add speed and altitude

--- GetCarSwift/DataViewController.swift
+++ GetCarSwift/DataViewController.swift
@@ -9,51 +9,57 @@
 import UIKit
 import CoreMotion
 
-class DataViewController: UIViewController {
+class DataViewController: UIViewController, CLLocationManagerDelegate {
     
-    @IBOutlet weak var maxData: UILabel!
-    @IBOutlet weak var dataLabel: UILabel!
+    @IBOutlet weak var vLabel: UILabel!
+    @IBOutlet weak var aLabel: UILabel!
+    @IBOutlet weak var altitude: UILabel!
+    
     let motionManager = CMMotionManager()
-    
-    var maxX: Double = -Double.infinity
-    var maxY: Double = -Double.infinity
-    var maxZ: Double = -Double.infinity
+    let locationManager = CLLocationManager()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
         guard motionManager.accelerometerAvailable else {
-            dataLabel.text = "此设备不支持加速度传感器"
+            aLabel.text = "此设备不支持加速度传感器"
+            return
+        }
+        
+        guard CLLocationManager.locationServicesEnabled() else {
+            vLabel.text = "此设备不支持GPS"
             return
         }
 
         motionManager.accelerometerUpdateInterval = 0.01
-
-    }
-    
-    override func viewDidAppear(animated: Bool) {
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.delegate = self
+        
         motionManager.startAccelerometerUpdatesToQueue(NSOperationQueue(), withHandler: { (data, error) in
             guard let validData = data else {
                 return
             }
             
-            
             dispatch_async(dispatch_get_main_queue(), {
                 let curX = validData.acceleration.x
                 let curY = validData.acceleration.y
                 let curZ = validData.acceleration.z
-                self.maxX = max(curX, self.maxX)
-                self.maxY = max(curY, self.maxY)
-                self.maxZ = max(curZ, self.maxZ)
-                self.dataLabel.text = String(format: "x : %.2f, y : %.2f, z : %.2f", curX, curY, curZ)
-                self.maxData.text = String(format: "max x : %.2f, max y : %.2f, max z : %.2f", self.maxX, self.maxY, self.maxZ)
+                self.aLabel.text = String(format: "加速度: x=%.2f, y=%.2f, z=%.2f", curX, curY, curZ)
             })
             
         })
+        locationManager.startUpdatingLocation()
+
     }
     
-    override func viewDidDisappear(animated: Bool) {
-        motionManager.stopAccelerometerUpdates()
+    func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let location = locations.last else {
+            return
+        }
+        dispatch_async(dispatch_get_main_queue(), {
+            self.vLabel.text = String(format: "速度: %.2f km/h", location.speed > 0 ? location.speed * 3.6 : 0)
+            self.altitude.text = String(format: "海拔: %i m", abs(location.altitude) > 20000 ? 0 : location.altitude)
+        })
     }
 
 }
