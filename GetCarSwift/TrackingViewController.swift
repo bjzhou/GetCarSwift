c8b2da12e8ef9666317d4775a633dc6fb0e286e8
bjzhou <bjzhou1990@gmail.com>
2015-08-09 22:17:50 +0800

    add tracking ui

---
+++ GetCarSwift/TrackingViewController.swift
@@ -0,0 +1,22 @@
+//
+//  TrackingViewController.swift
+//  GetCarSwift
+//
+//  Created by 周斌佳 on 15/8/9.
+//  Copyright © 2015年 &#21608;&#25996;&#20339;. All rights reserved.
+//
+
+import UIKit
+
+class TrackingViewController: UIViewController {
+    @IBOutlet weak var scrollView: UIScrollView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func viewDidLayoutSubviews() {
+        scrollView.contentSize = CGSizeMake(scrollView.frame.size.width, 667)
+    }
+
+}
